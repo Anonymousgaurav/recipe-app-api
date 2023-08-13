a712988b1d6050a6a17caee184c882170fe93e07
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-08-13 11:06:16 +0530

    code updated models and admin django

--- core/models.py
+++ core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """ **extra_field means user can provide keyword arguments """
+
+    def create_user(self, email, password=None, **extra_field):
+        if not email:
+            raise ValueError('User Must HAvr Email Address')
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """ create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+""" PermissionsMixin is for Functionality for the permissions and fields """
+""" AbstractBaseUser is for Functionality for the auth system """
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=25, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
